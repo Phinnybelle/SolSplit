aade741c5008272327ade6ba235890bf6d1e1457
Ekong Jemimah <ekongjemimah@gmail.com>
2025-09-19 00:13:06 +0100

    Initial React project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AddBillEmpty from "./components/AddBillEmpty";
+import AddBillFilled from "./components/AddBillFilled";
+import SplitBill from "./components/SplitBill";
+import Insights from "./components/pages/Insights"; // new page
+import SpendingChart from "./components/SpendingChart"; // optional chart
 
 function App() {
+  const [screen, setScreen] = useState(1);
+
+  // Store the bill data here
+  const [bill, setBill] = useState({
+    description: "",
+    amount: "",
+    group: ""
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black text-white min-h-screen flex flex-col">
+      {/* Navbar */}
+      {/* <Navbar onNavigate={setScreen} /> */}
+
+      {/* Main Content */}
+      <div className="flex-1 overflow-y-auto">
+        {screen === 1 && (
+          <AddBillEmpty
+            bill={bill}
+            setBill={setBill}
+            onNext={() => setScreen(2)}
+          />
+        )}
+        {screen === 2 && (
+          <AddBillFilled bill={bill} onNext={() => setScreen(3)} />
+        )}
+        {screen === 3 && <SplitBill bill={bill} />}
+        {screen === 4 && <Insights />} {/* new insights page */}
+      </div>
     </div>
   );
 }
 
 export default App;
+// filepath: c:\Users\PROGRESSIVE\bill-splitter\src\App.js
